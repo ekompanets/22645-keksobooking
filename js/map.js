b0985d9022613fe73db457cb3755638d356080b7
ekompanets <amelie.kom@gmail.com>
2017-04-12 15:10:45 +0300

    form validation

--- js/map.js
+++ js/map.js
@@ -244,9 +244,9 @@ var checkOut = form.querySelector('#timeout');
 
 // соответствие времени
 var CHECK_MAP = {
-  'in_12' : 'out_12',
-  'in_13' : 'out_13',
-  'in_14' : 'out_14'
+  'in_12': 'out_12',
+  'in_13': 'out_13',
+  'in_14': 'out_14'
 };
 // установка выезда при изменении заезда
 checkIn.addEventListener('change', function (evt) {
@@ -255,9 +255,9 @@ checkIn.addEventListener('change', function (evt) {
 
 // соответствие стоимости
 var TYPES_MAP = {
-  'flat' : 1000,
-  'shack' : 0,
-  'palace' : 10000
+  'flat': 1000,
+  'shack': 0,
+  'palace': 10000
 };
 
 // установка цены при изменении типа жилья
@@ -267,9 +267,9 @@ accommodationType.addEventListener('change', function (evt) {
 
 // соответствие количества комнат и гостей
 var GUEST_MAP = {
-  'num_1' : 'guest_0',
-  'num_2' : 'guest_3',
-  'num_100' : 'guest_3'
+  'num_1': 'guest_0',
+  'num_2': 'guest_3',
+  'num_100': 'guest_3'
 };
 
 // установка rjkbxtcndf ujcntq при изменении кол-ва комнат
@@ -281,11 +281,11 @@ form.addEventListener('invalid', function (evt) {
   evt.target.classList.add('error');
 
   // снятие класса error при успешной валидации
-  evt.target.addEventListener('blur', function (evt) {
-    if (evt.target.classList.contains('error')) {
-      if (evt.target.validity.valid) {
-        evt.target.classList.remove('error');
+  evt.target.addEventListener('blur', function (evnt) {
+    if (evnt.target.classList.contains('error')) {
+      if (evnt.target.validity.valid) {
+        evnt.target.classList.remove('error');
       }
     }
-  })
+  });
 }, true);
