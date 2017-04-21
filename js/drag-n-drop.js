1b73f7a8024923742ff8868388e8e9fc73d2f463
ekompanets <amelie.kom@gmail.com>
2017-04-21 21:53:02 +0300

    Drag-n-drop

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -2,55 +2,52 @@
 'use strict';
 
 window.dragNDrop =(function () {
-	var setDragable = function (element, handler, callback) {
-		handler.addEventListener('mousedown', function (evt) {
-	    evt.preventDefault();
+  var setDragable = function (element, handler, callback) {
+    handler.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
 
-	    var startCoords = {
-	      x: evt.clientX,
-	      y: evt.clientY
-	    };
-	      console.log(startCoords.y+' - '+startCoords.x)
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+        console.log(startCoords.y + ' - ' + startCoords.x)
 
-	    var onMouseMove = function (moveEvt) {
-	      moveEvt.preventDefault();
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
 
-	      var shift = {
-	        x: startCoords.x - moveEvt.clientX,
-	        y: startCoords.y - moveEvt.clientY
-	      };
-	      
-	      startCoords = {
-	        x: moveEvt.clientX,
-	        y: moveEvt.clientY
-	      };
-	      var dif = {
-	      	dX: element.offsetLeft - shift.x,
-	      	dY: element.offsetTop - shift.y
-	      };
-	      if (callback(dif.dX, dif.dY)) {
-	      	element.style.top = dif.dY + 'px';
-	      	element.style.left = dif.dX + 'px';
-	      } else {
-	      	var event = new Event('mouseup');
-  			document.dispatchEvent(event);
-	      }
-	    };
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };        
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+        var dif = {
+          dX: element.offsetLeft - shift.x,
+          dY: element.offsetTop - shift.y
+        };
+        if (callback(dif.dX, dif.dY)) {
+          element.style.top = dif.dY + 'px';
+          element.style.left = dif.dX + 'px';
+        } else {
+          var event = new Event('mouseup');
+        document.dispatchEvent(event);
+        }
+      };
 
-	    var onMouseUp = function (upEvt) {
-	      upEvt.preventDefault();
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
 
-	      document.removeEventListener('mousemove', onMouseMove);
-	      document.removeEventListener('mouseup', onMouseUp);
-	    };
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
 
-
-	    document.addEventListener('mousemove', onMouseMove);
-	    document.addEventListener('mouseup', onMouseUp);
-	  });
-	}
-
-	return {
-		setDragable: setDragable
-	};
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
+  };
+  return {
+    setDragable: setDragable
+  };
 })();
--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 'use strict';
 
 (function () {
-  var pinMap = document.querySelector('.tokyo__pin-map');  
+  var pinMap = document.querySelector('.tokyo__pin-map');
 
   // отрисовываем пины
   window.pin.drawPins(window.lodge.createAds(), pinMap);
@@ -15,21 +15,21 @@
   // получение координат, на которые указывает .pin__main
   var getAddress = function (posX, posY) {
     return {
-      x: Math.floor(posX + adData.PIN_MAIN_WIDTH / 2) ,
-      y: (posY + adData.PIN_MAIN_HEIGHT)
-    }
+      x: Math.floor(posX + window.adData.PIN_MAIN_WIDTH / 2),
+      y: (posY + window.adData.PIN_MAIN_HEIGHT)
+    };
   };
-  // делаем .pin__main перетягиваемым, 
+  // делаем .pin__main перетягиваемым,
   // в callback передаем функцию, которая получает координаты пина и передает их в поле "адрес" формы
   window.dragNDrop.setDragable(pinMain, pinMain, function (posX, posY) {
     var adr = getAddress(posX, posY);
     // проверяем координаты пина на вхождение в допустимый диапазон
     // "x": координата x метки на карте в блоке .tokyo__pin-map от 300 до 900,
     // "y": координата y метки на карте в блоке .tokyo__pin-map от 100 до 500
-    // В случае, если пин вышел за пределы допустимой области, 
+    // В случае, если пин вышел за пределы допустимой области,
     // callback-функция возвращает false, чтобы остановить движение пина
     var canMove = false;
-    if ((adr.x >= adData.MIN_X_LOCATION) && (adr.x <= adData.MAX_X_LOCATION) && (adr.y >= adData.MIN_Y_LOCATION) && (adr.y <= adData.MAX_Y_LOCATION)) {
+    if ((adr.x >= window.adData.MIN_X_LOCATION) && (adr.x <= window.adData.MAX_X_LOCATION) && (adr.y >= window.adData.MIN_Y_LOCATION) && (adr.y <= window.adData.MAX_Y_LOCATION)) {
       address.value = 'x: ' + adr.x + ', y: ' + adr.y;
       canMove = true;
     }
