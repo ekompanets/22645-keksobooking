7a8062556a60dd2ec08d2232881eaa9c4891437a
ekompanets <amelie.kom@gmail.com>
2017-04-24 21:13:47 +0300

    callbacks

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -4,14 +4,8 @@
 window.synchronizeFields = (function () {
   return function (fieldIn, fieldOut, arrayIn, arrayOut, callback) {
     fieldIn.addEventListener('change', function (evt) {
-      var result = null;
-      for (var i = 0; i < arrayIn.length; i++) {
-
-        if (arrayIn[i] === fieldIn.value) {
-          result = arrayOut[i];
-        }
-      }
-      callback(fieldOut, result);
+      var index = arrayIn.indexOf(fieldIn.value);
+      callback(fieldOut, arrayOut[index]);
     });
   };
 })();
